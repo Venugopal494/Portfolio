1f72bc219fe42405b83d4749b7ae37b4d2730b44
unknown <mekala_venugopal@srmap.edu.in>
2021-10-29 11:37:49 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const programingskills = [
+    {
+      id: 1,
+      title: "python",
+      img:
+        " ./assets/python.jpeg",
+    },
+    {
+      id: 2,
+      title: "C",
+      img:
+        "./assets/C.png",
+    },
+    {
+      id: 3,
+      title: "java",
+      img:
+        "./assets/java.jpeg",
+    },
+  ];
+  
+  export const webdevelopmentskills = [
+    {
+      id: 1,
+      title: "HTML",
+      img: "./assets/html.png",
+    },
+    {
+      id: 2,
+      title: "CSS",
+      img: "./assets/css.png",
+    },
+    {
+      id: 3,
+      title: "Java Script",
+      img: "./assets/js.png",
+    },
+    {
+      id: 4,
+      title: "MySQL",
+      img: "./assets/Mysql.png",
+    },
+    {
+      id: 5,
+      title: "PHP",
+      img: "./assets/php.png",
+    },
+  ];
+  
+  export const SubjectSkills = [
+    {
+      id: 1,
+      title: "OOPS",
+      img:
+        "./assets/oop.jpeg",
+    },
+    {
+      id: 2,
+      title: "DSA",
+      img:
+        "./assets/DSA.jpeg",
+    },
+    {
+      id: 3,
+      title: "DBMS",
+      img:
+        "./assets/DBMS.png",
+    },
+    {
+      id: 4,
+      title: "Cyber Security",
+      img:
+        "./assets/CYBER.jpeg",
+    },
+  ];
+  
